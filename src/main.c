2211c6ce95892f42c5f1497b35594bd13e73cef9
Disperseur <disperseur.code@gmail.com>
2025-05-25 11:00:57 +0200

    Led status ok avec thread

--- src/main.c
+++ src/main.c
@@ -4,10 +4,23 @@
 #include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+
 
 #include <math.h>
 
+#define LED_RED 	DT_ALIAS(led0)
+#define LED_GREEN 	DT_ALIAS(led1)
+#define LED_BLUE 	DT_ALIAS(led2)
+
+
+
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
+
+typedef enum {RUNNING, WAITING, FAULT} _board_status_t;
+_board_status_t board_status = RUNNING;
+
 
 typedef struct _accel_t {
 	int64_t ax;
@@ -36,19 +49,35 @@ struct sensor_value val_temperature;
 struct sensor_value val_humidity;
 struct sensor_value acc[3], gyr[3];
 
+const struct gpio_dt_spec led_red 			= GPIO_DT_SPEC_GET(LED_RED, gpios);
+const struct gpio_dt_spec led_green 		= GPIO_DT_SPEC_GET(LED_GREEN, gpios);
+const struct gpio_dt_spec led_blue 			= GPIO_DT_SPEC_GET(LED_BLUE, gpios);
 
-const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-const struct device *sensor_pressure = DEVICE_DT_GET_ONE(st_lps22hb_press);
-const struct device *sensor_temperature = DEVICE_DT_GET_ONE(renesas_hs300x);
-const struct device *sensor_acceleration = DEVICE_DT_GET_ONE(bosch_bmi270);
+const struct device *const dev 				= DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+const struct device *sensor_pressure 		= DEVICE_DT_GET_ONE(st_lps22hb_press);
+const struct device *sensor_temperature 	= DEVICE_DT_GET_ONE(renesas_hs300x);
+const struct device *sensor_acceleration 	= DEVICE_DT_GET_ONE(bosch_bmi270);
+
+// timers
+// struct k_timer timer_led;
+
+
+// sems
+struct k_sem sem_board_status;
+
+
+// threads
+K_THREAD_STACK_DEFINE(board_status_led_thread_stack_area, 1024);
+struct k_thread board_status_led_thread_data;
 
 
 
 void setup_sensor_acceleration(const struct device *sensor_acceleration);
-
+void _board_status_led(void*, void*, void*);
 
 int main(void)
 {
+	int ret, ret_sensor_pressure, ret_sensor_temperature, ret_sensor_acceleration;
 	// config capteurs et console
 	
 	uint32_t dtr = 0;
@@ -58,6 +87,14 @@ int main(void)
 	}
 
 	
+	while (!(gpio_is_ready_dt(&led_red) && gpio_is_ready_dt(&led_green) && gpio_is_ready_dt(&led_blue))) {
+		LOG_ERR("RGB LED not ready");
+		k_sleep(K_MSEC(100));
+	}
+
+	gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&led_blue, GPIO_OUTPUT_INACTIVE);
 
 	/* Poll if the DTR flag was set */
 	while (!dtr) {
@@ -83,16 +120,23 @@ int main(void)
 
 	LOG_INF("Sensors ready.");
 
-	
-
-
-	
 	setup_sensor_acceleration(sensor_acceleration);
+
+
+	// sems init
+	k_sem_init(&sem_board_status, 1, 1);
+
+	// timers setup and startup
+	// k_timer_init(&timer_led, _cb_tim_status_led, NULL);
+	// k_timer_start(&timer_led, K_MSEC(500), K_MSEC(500));
+
+	// demarrage thread leds
 	
 
-		
-	int ret_sensor_pressure, ret_sensor_temperature, ret_sensor_acceleration;
-    
+	k_tid_t my_tid = k_thread_create(&board_status_led_thread_data, board_status_led_thread_stack_area, K_THREAD_STACK_SIZEOF(board_status_led_thread_stack_area), _board_status_led, NULL, NULL, NULL, 10, 0, K_NO_WAIT);
+
+	LOG_INF("Thread LED created");
+
 	while (1) {
 		//mesures
     	ret_sensor_pressure= sensor_sample_fetch(sensor_pressure);
@@ -131,16 +175,16 @@ int main(void)
 		}
 
 		// conversion
-		sensors_data.temperature = sensor_value_to_milli(&val_temperature);
-		sensors_data.humidity = sensor_value_to_milli(&val_humidity);
-		sensors_data.pressure = sensor_value_to_milli(&val_pressure);
-		sensors_data.altitude = 1000.0 * 44330.0 * ( 1.0 - pow(sensor_value_to_double(&val_pressure)/101.325, 1/5.255) );
-		sensors_data.accel.ax = sensor_value_to_milli(&acc[0]);
-		sensors_data.accel.ay = sensor_value_to_milli(&acc[1]);
-		sensors_data.accel.az = sensor_value_to_milli(&acc[2]);
-		sensors_data.gyro.gx = sensor_value_to_milli(&gyr[0]);
-		sensors_data.gyro.gy = sensor_value_to_milli(&gyr[1]);
-		sensors_data.gyro.gz = sensor_value_to_milli(&gyr[2]);
+		sensors_data.temperature 	= sensor_value_to_milli(&val_temperature);
+		sensors_data.humidity 		= sensor_value_to_milli(&val_humidity);
+		sensors_data.pressure 		= sensor_value_to_milli(&val_pressure);
+		sensors_data.altitude 		= 1000.0 * 44330.0 * ( 1.0 - pow(sensor_value_to_double(&val_pressure)/101.325, 1/5.255) );
+		sensors_data.accel.ax 		= sensor_value_to_milli(&acc[0]);
+		sensors_data.accel.ay 		= sensor_value_to_milli(&acc[1]);
+		sensors_data.accel.az 		= sensor_value_to_milli(&acc[2]);
+		sensors_data.gyro.gx 		= sensor_value_to_milli(&gyr[0]);
+		sensors_data.gyro.gy 		= sensor_value_to_milli(&gyr[1]);
+		sensors_data.gyro.gz 		= sensor_value_to_milli(&gyr[2]);
 
 
 		// affichage en millieme de l'unite correspondante pour eviter les flottants
@@ -153,9 +197,50 @@ int main(void)
 		printk("\n");
 
 		k_sleep(K_SECONDS(1));
+
+		//k_sem_take(&sem_board_status, K_FOREVER);
+		board_status = FAULT;
+		//k_sem_give(&sem_board_status);
+
+		printk("bs=%d", board_status);
 	}
 }
 
+
+
+
+
+void _board_status_led(void*, void*, void*) {
+	for(;;) {
+		//callback to display the status of the program on the RGB led
+		//k_sem_take(&sem_board_status, K_FOREVER);
+		_board_status_t bs = board_status;
+		//k_sem_give(&sem_board_status);
+
+		switch (bs)
+		{
+		case RUNNING:
+			gpio_pin_toggle_dt(&led_green);
+			break;
+
+		case WAITING:
+			gpio_pin_set_dt(&led_green, 1);
+			break;
+
+		case FAULT:
+			gpio_pin_toggle_dt(&led_red);
+			break;
+		
+		default:
+			gpio_pin_set_dt(&led_red, 0);
+			gpio_pin_set_dt(&led_green, 0);
+			gpio_pin_set_dt(&led_blue, 1);
+			break;
+		}
+
+		k_sleep(K_MSEC(300));
+	}
+}
 
 
 void setup_sensor_acceleration(const struct device *sensor_acceleration) {
