4bac0a4f199159ae46459487359599816d5366e5
Disperseur <disperseur.code@gmail.com>
2025-05-29 20:56:06 +0200

    debut structure board_t

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@ Pour avoir le temps : k_uptime_get_32()
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/sensor.h>
-// #include <zephyr/logging/log.h>
 #include <zephyr/drivers/gpio.h>
 #include <math.h>
 
@@ -22,11 +21,8 @@ Pour avoir le temps : k_uptime_get_32()
 #define COMMAND_BUFFER_SIZE 100
 
 
-// LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
-
 
 typedef enum {RUNNING, WAITING, FAULT} _board_status_t;
-_board_status_t board_status = RUNNING;
 
 
 typedef struct _accel_t {
@@ -50,26 +46,71 @@ typedef struct _data_t {
 	gyro_t  gyro;
 } data_t;
 
-data_t sensors_data;
+
+
+
+typedef struct _rgbled_t {
+	struct gpio_dt_spec led_red;
+	struct gpio_dt_spec led_green;
+	struct gpio_dt_spec led_blue;
+} rgbled_t;
+
+typedef struct _devices_t {
+	const struct device *sensor_pressure;
+	const struct device *sensor_temperature;
+	const struct device *sensor_acceleration;
+} devices_t;
+
+
+typedef struct _sensors_t {
+	data_t data;
+	devices_t devices;
+
+	unsigned int sensors_period;
+} sensors_t;
+
+typedef struct _board_t {
+	rgbled_t led;
+	sensors_t sensors;
+
+	const struct device *console;
+
+	char command_buffer[COMMAND_BUFFER_SIZE];
+
+	_board_status_t status;
+} board_t;
+
+
+
 struct sensor_value val_pressure;
 struct sensor_value val_temperature;
 struct sensor_value val_humidity;
 struct sensor_value acc[3], gyr[3];
 
-const struct gpio_dt_spec led_red 			= GPIO_DT_SPEC_GET(LED_RED, gpios);
-const struct gpio_dt_spec led_green 		= GPIO_DT_SPEC_GET(LED_GREEN, gpios);
-const struct gpio_dt_spec led_blue 			= GPIO_DT_SPEC_GET(LED_BLUE, gpios);
-
-const struct device *const dev 				= DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-const struct device *sensor_pressure 		= DEVICE_DT_GET_ONE(st_lps22hb_press);
-const struct device *sensor_temperature 	= DEVICE_DT_GET_ONE(renesas_hs300x);
-const struct device *sensor_acceleration 	= DEVICE_DT_GET_ONE(bosch_bmi270);
 
 
+board_t board = {
+	.led = {
+		.led_red 					= GPIO_DT_SPEC_GET(LED_RED, gpios),
+		.led_green 					= GPIO_DT_SPEC_GET(LED_GREEN, gpios),
+		.led_blue 					= GPIO_DT_SPEC_GET(LED_BLUE, gpios)
+	},
 
-unsigned int sensors_period = 500; //ms
+	.sensors = {
+		.devices = {
+			.sensor_pressure 		= DEVICE_DT_GET_ONE(st_lps22hb_press),
+			.sensor_temperature 	= DEVICE_DT_GET_ONE(renesas_hs300x),
+			.sensor_acceleration 	= DEVICE_DT_GET_ONE(bosch_bmi270)
+		},
 
-char command_buffer[COMMAND_BUFFER_SIZE];
+		.sensors_period 			= 500
+	},
+
+	.console 						= DEVICE_DT_GET(DT_CHOSEN(zephyr_console)),
+	.status							= RUNNING
+};
+
+
 
 
 
@@ -86,6 +127,15 @@ struct k_thread measures_thread_data;
 struct k_sem sem_cmd;
 struct k_sem sem_measures;
 
+
+
+
+
+
+
+
+
+
 void setup_sensor_acceleration(const struct device *sensor_acceleration);
 
 void _handler_cmd(void*, void*, void*);
@@ -93,7 +143,7 @@ void _handler_measures(void*, void*, void*);
 
 void _cb_timer_status_led(struct k_timer *tim);
 void _cb_timer_measures(struct k_timer *tim);
-void _cb_uart_rx(const struct device *dev, void *user_data);
+void _cb_uart_rx(const struct device *console, void *user_data);
 
 
 
@@ -101,13 +151,13 @@ void _cb_uart_rx(const struct device *dev, void *user_data);
 int main(void)
 {
 	// on config en premier la led pour avoir un retour dans le max de cas
-	while (!(gpio_is_ready_dt(&led_red) && gpio_is_ready_dt(&led_green) && gpio_is_ready_dt(&led_blue))) {
+	while (!(gpio_is_ready_dt(&board.led.led_red) && gpio_is_ready_dt(&board.led.led_green) && gpio_is_ready_dt(&board.led.led_blue))) {
 		k_sleep(K_MSEC(100));
 	}
 
-	gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE);
-	gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_INACTIVE);
-	gpio_pin_configure_dt(&led_blue, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&board.led.led_red, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&board.led.led_green, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&board.led.led_blue, GPIO_OUTPUT_INACTIVE);
 
 	// timers setup and startup
 	k_timer_init(&timer_led, _cb_timer_status_led, NULL);
@@ -118,44 +168,44 @@ int main(void)
 	uint32_t dtr = 0;
 	if (usb_enable(NULL)) {
 		printk("Failed to start USB\n");
-		board_status = FAULT;
+		board.status = FAULT;
 		while(1);
 	}
-	board_status = RUNNING;
+	board.status = RUNNING;
 
 	/* Poll if the DTR flag was set: there is a client at the other side of the serial port */
 	while (!dtr) {
 		/* Poll if the DTR flag was set */
-		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		uart_line_ctrl_get(board.console, UART_LINE_CTRL_DTR, &dtr);
 		/* Give CPU resources to low priority threads. */
-		board_status = FAULT;
+		board.status = FAULT;
 		k_sleep(K_MSEC(100));
 	}
-	board_status = RUNNING;
+	board.status = RUNNING;
 
 	// uart irq setup
-	uart_irq_callback_set(dev, _cb_uart_rx);
-	uart_irq_rx_enable(dev);
+	uart_irq_callback_set(board.console, _cb_uart_rx);
+	uart_irq_rx_enable(board.console);
 
 	printk("Waiting for sensors to be ready...\n");
 
-	while (!device_is_ready(sensor_pressure)) {
-		board_status = FAULT;
-		printk("Device %s is not ready\n", sensor_pressure->name);
+	while (!device_is_ready(board.sensors.devices.sensor_pressure)) {
+		board.status = FAULT;
+		printk("Device %s is not ready\n", board.sensors.devices.sensor_pressure->name);
 		k_sleep(K_SECONDS(1));
 	}
-	while (!device_is_ready(sensor_temperature)) {
-		printk("Device %s is not ready\n", sensor_temperature->name);
+	while (!device_is_ready(board.sensors.devices.sensor_temperature)) {
+		printk("Device %s is not ready\n", board.sensors.devices.sensor_temperature->name);
 		k_sleep(K_SECONDS(1));
 	}
-	while (!device_is_ready(sensor_acceleration)) {
-		printk("Device %s is not ready\n", sensor_acceleration->name);
+	while (!device_is_ready(board.sensors.devices.sensor_acceleration)) {
+		printk("Device %s is not ready\n", board.sensors.devices.sensor_acceleration->name);
 		k_sleep(K_SECONDS(1));
 	}
 
 	printk("Sensors ready.\n");
 
-	setup_sensor_acceleration(sensor_acceleration);
+	setup_sensor_acceleration(board.sensors.devices.sensor_acceleration);
 
 
 	k_sem_init(&sem_cmd, 0, 1);
@@ -167,7 +217,7 @@ int main(void)
 
 
 	k_timer_init(&timer_measures, _cb_timer_measures, NULL);
-	k_timer_start(&timer_measures, K_MSEC(sensors_period), K_MSEC(sensors_period));
+	k_timer_start(&timer_measures, K_MSEC(board.sensors.sensors_period), K_MSEC(board.sensors.sensors_period));
 	
 }
 
@@ -179,37 +229,37 @@ void _handler_measures(void*, void*, void*) {
 		k_sem_take(&sem_measures, K_FOREVER);
 
 
-		ret= sensor_sample_fetch(sensor_pressure);
+		ret= sensor_sample_fetch(board.sensors.devices.sensor_pressure);
 		
 		if(ret != 0) printk("failed to fetch pressure sensor: %d\n", ret);
 		else {
-			ret = sensor_channel_get(sensor_pressure, SENSOR_CHAN_PRESS, &val_pressure);
+			ret = sensor_channel_get(board.sensors.devices.sensor_pressure, SENSOR_CHAN_PRESS, &val_pressure);
 			
 			if(ret != 0) printk("failed to get pressure: %d\n", ret);
 		}
 
-		ret = sensor_sample_fetch(sensor_temperature);
+		ret = sensor_sample_fetch(board.sensors.devices.sensor_temperature);
 
 		if(ret != 0) printk("failed to fetch temperature sensor: %d\n", ret);
 		else {
-			ret = sensor_channel_get(sensor_temperature, SENSOR_CHAN_AMBIENT_TEMP, &val_temperature);
+			ret = sensor_channel_get(board.sensors.devices.sensor_temperature, SENSOR_CHAN_AMBIENT_TEMP, &val_temperature);
 
 			if(ret != 0) printk("failed to get temperature: %d\n", ret);
 		
-			ret = sensor_channel_get(sensor_temperature, SENSOR_CHAN_HUMIDITY, &val_humidity);
+			ret = sensor_channel_get(board.sensors.devices.sensor_temperature, SENSOR_CHAN_HUMIDITY, &val_humidity);
 			
 			if(ret != 0) printk("failed to get humidity: %d\n", ret);
 		}
 
-		ret = sensor_sample_fetch(sensor_acceleration);
+		ret = sensor_sample_fetch(board.sensors.devices.sensor_acceleration);
 
 		if(ret != 0) printk("failed to fetch acceleration sensor: %d\n", ret);
 		else {
-			ret = sensor_channel_get(sensor_acceleration, SENSOR_CHAN_ACCEL_XYZ, acc);
+			ret = sensor_channel_get(board.sensors.devices.sensor_acceleration, SENSOR_CHAN_ACCEL_XYZ, acc);
 			
 			if(ret != 0) printk("failed to get acc: %d\n", ret);
 			
-			ret = sensor_channel_get(sensor_acceleration, SENSOR_CHAN_GYRO_XYZ, gyr);
+			ret = sensor_channel_get(board.sensors.devices.sensor_acceleration, SENSOR_CHAN_GYRO_XYZ, gyr);
 			
 			if(ret != 0) printk("failed to get gyr: %d\n", ret);
 		}
@@ -217,25 +267,25 @@ void _handler_measures(void*, void*, void*) {
 		timestamp = k_uptime_get_32();
 
 		// conversion
-		sensors_data.temperature 	= sensor_value_to_milli(&val_temperature);
-		sensors_data.humidity 		= sensor_value_to_milli(&val_humidity);
-		sensors_data.pressure 		= sensor_value_to_milli(&val_pressure);
-		sensors_data.altitude 		= 1000.0 * 44330.0 * ( 1.0 - pow(sensor_value_to_double(&val_pressure)/101.325, 1/5.255) );
-		sensors_data.accel.ax 		= sensor_value_to_milli(&acc[0]);
-		sensors_data.accel.ay 		= sensor_value_to_milli(&acc[1]);
-		sensors_data.accel.az 		= sensor_value_to_milli(&acc[2]);
-		sensors_data.gyro.gx 		= sensor_value_to_milli(&gyr[0]);
-		sensors_data.gyro.gy 		= sensor_value_to_milli(&gyr[1]);
-		sensors_data.gyro.gz 		= sensor_value_to_milli(&gyr[2]);
+		board.sensors.data.temperature 	= sensor_value_to_milli(&val_temperature);
+		board.sensors.data.humidity 		= sensor_value_to_milli(&val_humidity);
+		board.sensors.data.pressure 		= sensor_value_to_milli(&val_pressure);
+		board.sensors.data.altitude 		= 1000.0 * 44330.0 * ( 1.0 - pow(sensor_value_to_double(&val_pressure)/101.325, 1/5.255) );
+		board.sensors.data.accel.ax 		= sensor_value_to_milli(&acc[0]);
+		board.sensors.data.accel.ay 		= sensor_value_to_milli(&acc[1]);
+		board.sensors.data.accel.az 		= sensor_value_to_milli(&acc[2]);
+		board.sensors.data.gyro.gx 		= sensor_value_to_milli(&gyr[0]);
+		board.sensors.data.gyro.gy 		= sensor_value_to_milli(&gyr[1]);
+		board.sensors.data.gyro.gz 		= sensor_value_to_milli(&gyr[2]);
 
 
 		//affichage en millieme de l'unite correspondante pour eviter les flottants
-		printk("%d TEMPERATURE %lld\n", timestamp, sensors_data.temperature);
-		printk("%d HUMIDITY %lld\n", timestamp, sensors_data.humidity);
-		printk("%d PRESSURE %lld\n", timestamp, sensors_data.pressure);
-		printk("%d ALTITUDE %lld\n", timestamp, sensors_data.altitude);
-		printk("%d ACCEL_LIN %lld %lld %lld\n", timestamp, sensors_data.accel.ax, sensors_data.accel.ay, sensors_data.accel.az);
-		printk("%d ACCEL_ROT %lld %lld %lld\n", timestamp, sensors_data.gyro.gx,  sensors_data.gyro.gy,  sensors_data.gyro.gz);		
+		printk("%d TEMPERATURE %lld\n", timestamp, board.sensors.data.temperature);
+		printk("%d HUMIDITY %lld\n", timestamp, board.sensors.data.humidity);
+		printk("%d PRESSURE %lld\n", timestamp, board.sensors.data.pressure);
+		printk("%d ALTITUDE %lld\n", timestamp, board.sensors.data.altitude);
+		printk("%d ACCEL_LIN %lld %lld %lld\n", timestamp, board.sensors.data.accel.ax, board.sensors.data.accel.ay, board.sensors.data.accel.az);
+		printk("%d ACCEL_ROT %lld %lld %lld\n", timestamp, board.sensors.data.gyro.gx,  board.sensors.data.gyro.gy,  board.sensors.data.gyro.gz);		
 		printk("\n");
 	}
 }
@@ -249,7 +299,7 @@ void _handler_cmd(void*, void*, void*) {
 
 		printk("ACK\n");
 		// une commande est dispo, on l'affiche -> il faudra la récup pour switch dessus a posteriori
-		printk("%s", command_buffer);
+		printk("%s", board.command_buffer);
 	}
 }
 
@@ -261,45 +311,45 @@ void _cb_timer_measures(struct k_timer *tim) {
 void _cb_timer_status_led(struct k_timer *tim) {
 	//callback to display the status of the program on the RGB led
 
-	switch (board_status)
+	switch (board.status)
 	{
 	case RUNNING:
-		gpio_pin_set_dt(&led_red, 0);
-		gpio_pin_set_dt(&led_blue, 0);
+		gpio_pin_set_dt(&board.led.led_red, 0);
+		gpio_pin_set_dt(&board.led.led_blue, 0);
 
-		gpio_pin_toggle_dt(&led_green);
+		gpio_pin_toggle_dt(&board.led.led_green);
 		break;
 
 	case WAITING:
-		gpio_pin_set_dt(&led_red, 0);
-		gpio_pin_set_dt(&led_blue, 0);
+		gpio_pin_set_dt(&board.led.led_red, 0);
+		gpio_pin_set_dt(&board.led.led_blue, 0);
 
-		gpio_pin_set_dt(&led_green, 1);
+		gpio_pin_set_dt(&board.led.led_green, 1);
 		break;
 
 	case FAULT:
-		gpio_pin_set_dt(&led_green, 0);
-		gpio_pin_set_dt(&led_blue, 0);
+		gpio_pin_set_dt(&board.led.led_green, 0);
+		gpio_pin_set_dt(&board.led.led_blue, 0);
 
-		gpio_pin_toggle_dt(&led_red);
+		gpio_pin_toggle_dt(&board.led.led_red);
 		break;
 	
 	default:
-		gpio_pin_set_dt(&led_red, 0);
-		gpio_pin_set_dt(&led_green, 0);
-		gpio_pin_set_dt(&led_blue, 1);
+		gpio_pin_set_dt(&board.led.led_red, 0);
+		gpio_pin_set_dt(&board.led.led_green, 0);
+		gpio_pin_set_dt(&board.led.led_blue, 1);
 		break;
 	}
 
 }
 
-void _cb_uart_rx(const struct device *dev, void *user_data) {
+void _cb_uart_rx(const struct device *console, void *user_data) {
 	// printk("ACK\n");
 
-	if(uart_irq_rx_ready(dev)) {
+	if(uart_irq_rx_ready(console)) {
 		// si data a lire dans la fifo uart
 		int i = 0;
-		while(uart_fifo_read(dev, &command_buffer[i], 1)) {
+		while(uart_fifo_read(console, &board.command_buffer[i], 1)) {
 			i++;
 		}
 	}
